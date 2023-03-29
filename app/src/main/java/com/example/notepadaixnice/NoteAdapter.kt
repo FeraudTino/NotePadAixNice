a58e3a61fed5c9c9c2f052c73310c192f2dab0c0
Alban Pelissier <alban@qodlab.io>
2023-03-29 15:08:33 +0200

    RecycleView Adapter

---
+++ app/src/main/java/com/example/notepadaixnice/NoteAdapter.kt
@@ -0,0 +1,38 @@
+package com.example.notepadaixnice
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.cardview.widget.CardView
+import androidx.recyclerview.widget.RecyclerView
+
+class NoteAdapter(val notes: List<Note>, val itemClickListener: View.OnClickListener)
+    : RecyclerView.Adapter<NoteAdapter.ViewHolder>() {
+
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val cardView = itemView.findViewById(R.id.card_view) as CardView
+        val titleView = cardView.findViewById(R.id.title) as TextView
+        val exerptView = cardView.findViewById(R.id.exerpt) as TextView
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val viewItem = LayoutInflater.from(parent.context)
+            .inflate(
+                R.layout.item_note, parent, false
+            )
+        return ViewHolder(viewItem)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val note = notes[position]
+        holder.cardView.setOnClickListener(itemClickListener)
+        holder.cardView.tag = position
+        holder.titleView.text = note.title
+        holder.exerptView.text = note.text
+    }
+
+    override fun getItemCount(): Int {
+        return notes.size
+    }
+}
--- app/src/main/java/com/example/notepadaixnice/NoteListActivity.kt
+++ app/src/main/java/com/example/notepadaixnice/NoteListActivity.kt
@@ -1,11 +1,37 @@
 package com.example.notepadaixnice
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
-class NoteListActivity : AppCompatActivity() {
+class NoteListActivity : AppCompatActivity(), View.OnClickListener {
+    lateinit var notes:MutableList<Note>
+    lateinit var adapter:NoteAdapter
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_note_list)
+
+        notes = mutableListOf<Note>()
+
+        notes.add(Note("Note 1", "Super cela fonctionne parfaitement"))
+        notes.add(Note("Memo M2I", "Prochaine formation à Aix et Nice ne sera pas en distanciel"))
+        notes.add(Note("Memo Foot", "La France sera la future equipe championne d'Europe devant nos amis Anglais!!!!"))
+        notes.add(Note("Pourquoi Kotlin ?", "Parce que c'est la formation mon petit !! "))
+
+        adapter= NoteAdapter(notes,this)
+
+        val recyclerView = findViewById<RecyclerView>(R.id.notes_recycle_view)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.adapter = adapter
+
+    }
+    override fun onClick(view: View) {
+        if (view.tag != null)
+            Log.i("NoteListActivity", "Note de ma list")
     }
 }
